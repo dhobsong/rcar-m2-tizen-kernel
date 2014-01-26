5f85049d96b20d314cf4cc7e84c1f6b582e0d12c
Ben Dooks <ben.dooks@codethink.co.uk>
2014-01-26 16:05:37 +0000

    i2c: rcar: fix NACK error code
    The response to a bus NACK is to return -ENXIO instead of the
    -EREMOTEIO being currently returned by the driver.
    
    Signed-off-by: Ben Dooks <ben.dooks@codethink.co.uk>
    Signed-off-by: Wolfram Sang <wsa@the-dreams.de>
    (cherry picked from commit 6ff4b105163223741f5a82804f9695221017ae42)
    (Queued by Wolfram Sang for mainline but not yet in Linus's tree)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>

--- drivers/i2c/busses/i2c-rcar.c
+++ drivers/i2c/busses/i2c-rcar.c
@@ -599,7 +599,7 @@ static int rcar_i2c_master_xfer(struct i2c_adapter *adap,
 		 * error handling
 		 */
 		if (rcar_i2c_flags_has(priv, ID_NACK)) {
-			ret = -EREMOTEIO;
+			ret = -ENXIO;
 			break;
 		}
 
@@ -618,7 +618,7 @@ static int rcar_i2c_master_xfer(struct i2c_adapter *adap,
 
 	pm_runtime_put(dev);
 
-	if (ret < 0 && ret != -EREMOTEIO)
+	if (ret < 0 && ret != -ENXIO)
 		dev_err(dev, "error %d : %x\n", ret, priv->flags);
 
 	return ret;
