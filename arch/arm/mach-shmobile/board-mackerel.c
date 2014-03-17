1057d29700b69eb4d713cdfaaa132cd9c4ef9d6c
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2014-03-16 17:37:06 -0700

    ASoC: fsi: fixup compile error for simple-card
    This patches fixes
    c7a507eea1db1430476289f525f9c853d5d485e8
    (ASoC: fsi: fixup SND_SOC_DAIFMT_CBx_CFx flags)
    commit's compie error
    
    arch/arm/mach-shmobile/board-mackerel.c:512:2: \
      error: unknown field 'fmt' specified in initializer
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    
    Conflicts:
    	arch/arm/mach-shmobile/board-mackerel.c
    
    (cherry picked from commit 354da5344ae0c595ac170340fbd5e2f7317a1635)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- arch/arm/mach-shmobile/board-mackerel.c
+++ arch/arm/mach-shmobile/board-mackerel.c
@@ -509,6 +509,7 @@ static struct asoc_simple_card_info fsi2_hdmi_info = {
 	.card		= "FSI2B-HDMI",
 	.codec		= "sh-mobile-hdmi",
 	.platform	= "sh_fsi2",
+	.daifmt		= SND_SOC_DAIFMT_CBS_CFS,
 	.cpu_dai = {
 		.name	= "fsib-dai",
 		.fmt	= SND_SOC_DAIFMT_CBM_CFM | SND_SOC_DAIFMT_IB_NF,
