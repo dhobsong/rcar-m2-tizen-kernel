7b0b6d9f74ac91a3e34641a311b3b691fd1bf600
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2014-03-20 16:57:22 +0900

    drm: rcar-du: Add no LVDS interface feature
    This is the function to choose the signal to HDMI transmitter.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_encoder.c
+++ drivers/gpu/drm/rcar-du/rcar_du_encoder.c
@@ -48,11 +48,17 @@ rcar_du_connector_best_encoder(struct drm_connector *connector)
 static void rcar_du_encoder_dpms(struct drm_encoder *encoder, int mode)
 {
 	struct rcar_du_encoder *renc = rcar_du_encoder(encoder);
+	unsigned int output;
 
 	if (renc->lvds)
 		rcar_du_lvdsenc_dpms(renc->lvds, encoder->crtc, mode);
 
-	if ((renc->output == RCAR_DU_OUTPUT_LVDS0) &&
+	if (rcar_du_has(renc->dev, RCAR_DU_FEATURE_NO_LVDS_INTERFACE))
+		output = RCAR_DU_OUTPUT_DPAD0;
+	else
+		output = RCAR_DU_OUTPUT_LVDS0;
+
+	if ((renc->output == output) &&
 			(get_rcar_slave_funcs(encoder)->dpms))
 		get_rcar_slave_funcs(encoder)->dpms(encoder, mode);
 }
@@ -113,11 +119,18 @@ static bool rcar_du_encoder_mode_fixup(struct drm_encoder *encoder,
 static void rcar_du_encoder_mode_prepare(struct drm_encoder *encoder)
 {
 	struct rcar_du_encoder *renc = rcar_du_encoder(encoder);
+	unsigned int output;
 
 	if (renc->lvds)
 		rcar_du_lvdsenc_dpms(renc->lvds, encoder->crtc,
 				     DRM_MODE_DPMS_OFF);
-	if ((renc->output == RCAR_DU_OUTPUT_LVDS0) &&
+
+	if (rcar_du_has(renc->dev, RCAR_DU_FEATURE_NO_LVDS_INTERFACE))
+		output = RCAR_DU_OUTPUT_DPAD0;
+	else
+		output = RCAR_DU_OUTPUT_LVDS0;
+
+	if ((renc->output == output) &&
 		(get_rcar_slave_funcs(encoder)->dpms))
 		get_rcar_slave_funcs(encoder)->dpms(encoder, DRM_MODE_DPMS_OFF);
 }
@@ -125,11 +138,18 @@ static void rcar_du_encoder_mode_prepare(struct drm_encoder *encoder)
 static void rcar_du_encoder_mode_commit(struct drm_encoder *encoder)
 {
 	struct rcar_du_encoder *renc = rcar_du_encoder(encoder);
+	unsigned int output;
 
 	if (renc->lvds)
 		rcar_du_lvdsenc_dpms(renc->lvds, encoder->crtc,
 				     DRM_MODE_DPMS_ON);
-	if ((renc->output == RCAR_DU_OUTPUT_LVDS0) &&
+
+	if (rcar_du_has(renc->dev, RCAR_DU_FEATURE_NO_LVDS_INTERFACE))
+		output = RCAR_DU_OUTPUT_DPAD0;
+	else
+		output = RCAR_DU_OUTPUT_LVDS0;
+
+	if ((renc->output == output) &&
 		(get_rcar_slave_funcs(encoder)->dpms))
 		get_rcar_slave_funcs(encoder)->dpms(encoder, DRM_MODE_DPMS_ON);
 }
@@ -139,10 +159,16 @@ static void rcar_du_encoder_mode_set(struct drm_encoder *encoder,
 				     struct drm_display_mode *adjusted_mode)
 {
 	struct rcar_du_encoder *renc = rcar_du_encoder(encoder);
+	unsigned int output;
 
 	rcar_du_crtc_route_output(encoder->crtc, renc->output);
 
-	if ((renc->output == RCAR_DU_OUTPUT_LVDS0) &&
+	if (rcar_du_has(renc->dev, RCAR_DU_FEATURE_NO_LVDS_INTERFACE))
+		output = RCAR_DU_OUTPUT_DPAD0;
+	else
+		output = RCAR_DU_OUTPUT_LVDS0;
+
+	if ((renc->output == output) &&
 		(get_rcar_slave_funcs(encoder)->mode_set))
 		get_rcar_slave_funcs(encoder)->mode_set(encoder,
 						 mode, adjusted_mode);
@@ -205,6 +231,7 @@ int rcar_du_encoder_init(struct rcar_du_device *rcdu,
 		return -ENOMEM;
 
 	renc->output = output;
+	renc->dev = rcdu;
 
 	switch (output) {
 	case RCAR_DU_OUTPUT_LVDS0:
