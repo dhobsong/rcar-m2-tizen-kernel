c2aacda907f74e049c064978f0a7b52df2aea185
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2014-03-24 13:34:40 +0900

    drm: Fix CRTC selector for FBDev access support
    The function to enable and the function to choose of
    FBDev's CRTC are separated into other entries.
    
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/drm_fb_helper.c
+++ drivers/gpu/drm/drm_fb_helper.c
@@ -813,9 +813,9 @@ int drm_fb_helper_set_par(struct fb_info *info)
 	}
 #if defined(CONFIG_DRM_FBDEV_CRTC)
 	disp_set_mode =
-		 fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC].mode_set.mode;
-	mode_set = &fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC].mode_set;
-	fb = fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC].mode_set.fb;
+		 fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC_NUM].mode_set.mode;
+	mode_set = &fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC_NUM].mode_set;
+	fb = fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC_NUM].mode_set.fb;
 
 	for (i = 0; i < fb_helper->connector_count; i++) {
 		if (fb_helper->connector_info[i]) {
@@ -912,7 +912,7 @@ int drm_fb_helper_set_par(struct fb_info *info)
 			goto mode_no_change;
 
 		ret = drm_mode_set_config_internal(&fb_helper->
-				crtc_info[CONFIG_DRM_FBDEV_CRTC].mode_set);
+				crtc_info[CONFIG_DRM_FBDEV_CRTC_NUM].mode_set);
 		if (ret) {
 			drm_modeset_unlock_all(dev);
 			return ret;
@@ -1075,7 +1075,7 @@ static int drm_fb_helper_single_fb_probe(struct drm_fb_helper *fb_helper,
 		desired_mode = fb_helper->crtc_info[i].desired_mode;
 
 #if defined(CONFIG_DRM_FBDEV_CRTC)
-		if ((desired_mode) && (i == CONFIG_DRM_FBDEV_CRTC)) {
+		if ((desired_mode) && (i == CONFIG_DRM_FBDEV_CRTC_NUM)) {
 #else
 		if (desired_mode) {
 #endif
@@ -1244,7 +1244,8 @@ void drm_fb_helper_fill_var(struct fb_info *info, struct drm_fb_helper *fb_helpe
 	info->var.width = -1;
 
 #if defined(CONFIG_DRM_FBDEV_CRTC)
-	drm_mode = fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC].mode_set.mode;
+	drm_mode =
+		 fb_helper->crtc_info[CONFIG_DRM_FBDEV_CRTC_NUM].mode_set.mode;
 	if (drm_mode) {
 		info->var.left_margin = drm_mode->htotal - drm_mode->hsync_end;
 		info->var.right_margin =
