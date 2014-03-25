60632c97efa2e4c001358aa42b349ead4aa51076
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2014-03-25 11:37:22 +0900

    drm: Connect live source to plane
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    (cherry picked from commit 614fe83883dfeb445da1e807f65ae05d1c6e5266)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- include/drm/drm_crtc.h
+++ include/drm/drm_crtc.h
@@ -649,6 +649,7 @@ struct drm_connector {
 struct drm_plane_funcs {
 	int (*update_plane)(struct drm_plane *plane,
 			    struct drm_crtc *crtc, struct drm_framebuffer *fb,
+			    struct drm_live_source *src,
 			    int crtc_x, int crtc_y,
 			    unsigned int crtc_w, unsigned int crtc_h,
 			    uint32_t src_x, uint32_t src_y,
@@ -689,6 +690,7 @@ struct drm_plane {
 
 	struct drm_crtc *crtc;
 	struct drm_framebuffer *fb;
+	struct drm_live_source *src;
 
 	/* CRTC gamma size for reporting to userspace */
 	uint32_t gamma_size;
@@ -709,6 +711,7 @@ struct drm_live_source_funcs {
 struct drm_live_source {
 	struct drm_device *dev;
 	struct list_head head;
+	struct list_head filp_head;
 
 	struct drm_mode_object base;
 
@@ -948,6 +951,7 @@ extern int drm_live_source_init(struct drm_device *dev,
 				const uint32_t *formats, uint32_t format_count,
 				const struct drm_live_source_funcs *funcs);
 extern void drm_live_source_cleanup(struct drm_live_source *src);
+extern void drm_live_sources_release(struct drm_file *priv);
 
 extern const char *drm_get_connector_name(const struct drm_connector *connector);
 extern const char *drm_get_connector_status_name(enum drm_connector_status status);
