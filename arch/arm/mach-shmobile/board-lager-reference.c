c10954f076f3bde6de262e197f4b4d7050fc54ca
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2014-04-10 09:26:53 +0900

    Local: ARM: shmobile: lager: enablve DVC on reference
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    (cherry picked from commit 514ce984c50b84570ae4a2f90f4e041489976b09)
    (not present upstream)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    (cherry picked from commit 2c9121efc1aec3bfa36aecd8db43729031766122)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- arch/arm/mach-shmobile/board-lager-reference.c
+++ arch/arm/mach-shmobile/board-lager-reference.c
@@ -94,7 +94,7 @@ static void __init lager_add_du_device(void)
 
 /* Sound */
 static struct rsnd_ssi_platform_info rsnd_ssi[] = {
-	RSND_SSI(AUDIOPP_DMAC_SLAVE_SCU0_TO_SSI0, gic_spi(370), 0),
+	RSND_SSI(AUDIOPP_DMAC_SLAVE_CMD0_TO_SSI0, gic_spi(370), 0),
 	RSND_SSI(AUDIOPP_DMAC_SLAVE_SSI1_TO_SCU1, gic_spi(371), RSND_SSI_CLK_PIN_SHARE),
 };
 
@@ -103,8 +103,11 @@ static struct rsnd_src_platform_info rsnd_src[2] = {
 	RSND_SRC(0, AUDIO_DMAC_SLAVE_SCU1_RX),
 };
 
+static struct rsnd_dvc_platform_info rsnd_dvc = {
+};
+
 static struct rsnd_dai_platform_info rsnd_dai = {
-	.playback = { .ssi = &rsnd_ssi[0], .src = &rsnd_src[0], },
+	.playback = { .ssi = &rsnd_ssi[0], .src = &rsnd_src[0], .dvc = &rsnd_dvc, },
 	.capture  = { .ssi = &rsnd_ssi[1], .src = &rsnd_src[1], },
 };
 
@@ -114,6 +117,8 @@ static struct rcar_snd_info rsnd_info = {
 	.ssi_info_nr	= ARRAY_SIZE(rsnd_ssi),
 	.src_info	= rsnd_src,
 	.src_info_nr	= ARRAY_SIZE(rsnd_src),
+	.dvc_info	= &rsnd_dvc,
+	.dvc_info_nr	= 1,
 	.dai_info	= &rsnd_dai,
 	.dai_info_nr	= 1,
 };
@@ -184,6 +189,7 @@ static const struct clk_name clk_names[] __initconst = {
 	{ "ssi1", "ssi.1", "rcar_sound" },
 	{ "src0", "src.0", "rcar_sound" },
 	{ "src1", "src.1", "rcar_sound" },
+	{ "dvc0", "dvc.0", "rcar_sound" },
 };
 
 /*
