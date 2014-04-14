1f1a672f4d0db6106347bbee9b5127691721e4fa
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2014-04-14 10:16:59 +0900

    Local: ARM: shmobile: r8a7791: enable DMA on DTSI
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
    (cherry picked from commit 88ec42138fa5ff87dc1355365c42bfc51874e194)
    (not present upstream)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    
    (cherry picked from commit 4fc091bf6a421c7a0f75c09bc71f3b077076d8f7)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- include/dt-bindings/clock/r8a7791-clock.h
+++ include/dt-bindings/clock/r8a7791-clock.h
@@ -61,6 +61,8 @@
 /* MSTP5 */
 #define R8A7791_CLK_THERMAL		22
 #define R8A7791_CLK_PWM			23
+#define R8A7791_CLK_DMA_LOW		2
+#define R8A7791_CLK_DMA_HI		1
 
 /* MSTP7 */
 #define R8A7791_CLK_EHCI		3
