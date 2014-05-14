722f4d978e77502c618f3f6cf8f1e2d4baf8451c
Kouei Abe <kouei.abe.cp@renesas.com>
2014-05-14 19:26:50 +0900

    sh_eth: r8a779x: Enable automatically fetch receive descriptor
    HDMAC automatically fetches the receive descriptor and receives frames.
    Continuous reception of multiple frames is possible.
    
    Signed-off-by: Kouei Abe <kouei.abe.cp@renesas.com>

--- drivers/net/ethernet/renesas/sh_eth.c
+++ drivers/net/ethernet/renesas/sh_eth.c
@@ -475,6 +475,7 @@ static struct sh_eth_cpu_data r8a779x_data = {
 	.eesr_err_check	= EESR_TWB | EESR_TABT | EESR_RABT | EESR_RFE |
 			  EESR_RDE | EESR_RFRMER | EESR_TFE | EESR_TDE |
 			  EESR_ECI,
+	.rmcr_value	= RMCR_RNC,
 
 	.apr		= 1,
 	.mpr		= 1,
