d1c109fd1d72cf84e46831af7280d48aca14c760
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2014-05-15 20:18:08 +0900

    ARM: shmobile: koelsch-reference: Add DU HDMI output support
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- arch/arm/mach-shmobile/board-koelsch-reference.c
+++ arch/arm/mach-shmobile/board-koelsch-reference.c
@@ -34,6 +34,12 @@
 
 /* DU */
 static struct rcar_du_encoder_data koelsch_du_encoders[] = {
+#if defined(CONFIG_DRM_ADV7511) || defined(CONFIG_DRM_ADV7511_MODULE)
+	{
+		.type = RCAR_DU_ENCODER_HDMI,
+		.output = RCAR_DU_OUTPUT_DPAD0,
+	},
+#endif
 	{
 		.type = RCAR_DU_ENCODER_NONE,
 		.output = RCAR_DU_OUTPUT_LVDS0,
