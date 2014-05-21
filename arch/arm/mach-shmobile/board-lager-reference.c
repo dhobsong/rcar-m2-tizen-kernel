bf8b1efe5b3fc07ad3700d0b9e0ed7d4bed9c08b
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2014-05-21 17:52:17 +0900

    ARM: shmobile: lager-reference: Add VIN driver support
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- arch/arm/mach-shmobile/board-lager-reference.c
+++ arch/arm/mach-shmobile/board-lager-reference.c
@@ -25,6 +25,7 @@
 #include <linux/init.h>
 #include <linux/of_gpio.h>
 #include <linux/of_platform.h>
+#include <linux/platform_data/camera-rcar.h>
 #include <linux/platform_data/rcar-du.h>
 #include <linux/platform_data/usb-rcar-gen2-phy.h>
 #include <linux/spi/flash.h>
@@ -37,6 +38,7 @@
 #include <mach/irqs.h>
 #include <mach/rcar-gen2.h>
 #include <mach/r8a7790.h>
+#include <media/soc_camera.h>
 #include <asm/mach/arch.h>
 #include <sound/rcar_snd.h>
 #include <sound/simple_card.h>
@@ -232,6 +234,8 @@ static const struct clk_name clk_names[] __initconst = {
 	{ "src0", "src.0", "rcar_sound" },
 	{ "src1", "src.1", "rcar_sound" },
 	{ "dvc0", "dvc.0", "rcar_sound" },
+	{ "vin0", NULL, "r8a7790-vin.0" },
+	{ "vin1", NULL, "r8a7790-vin.1" },
 	{ "vsp1-rt", NULL, "vsp1.0" },
 	{ "vsp1-sy", NULL, "vsp1.1" },
 	{ "vsp1-du0", NULL, "vsp1.2" },
@@ -455,6 +459,69 @@ static void __init lager_add_usb2_device(void)
 	platform_device_register_full(&pci2_info);
 };
 
+/* VIN */
+static const struct resource vin_resources[] __initconst = {
+	/* VIN0 */
+	DEFINE_RES_MEM(0xe6ef0000, 0x1000),
+	DEFINE_RES_IRQ(gic_spi(188)),
+	/* VIN1 */
+	DEFINE_RES_MEM(0xe6ef1000, 0x1000),
+	DEFINE_RES_IRQ(gic_spi(189)),
+};
+
+static void __init lager_add_vin_device(unsigned idx,
+					struct rcar_vin_platform_data *pdata)
+{
+	struct platform_device_info vin_info = {
+		.parent		= &platform_bus,
+		.name		= "r8a7790-vin",
+		.id		= idx,
+		.res		= &vin_resources[idx * 2],
+		.num_res	= 2,
+		.dma_mask	= DMA_BIT_MASK(32),
+		.data		= pdata,
+		.size_data	= sizeof(*pdata),
+	};
+
+	BUG_ON(idx > 1);
+
+	platform_device_register_full(&vin_info);
+}
+
+#define KOELSCH_CAMERA(idx, name, addr, pdata, flag)			\
+static struct i2c_board_info i2c_cam##idx##_device = {			\
+	I2C_BOARD_INFO(name, addr),					\
+};									\
+									\
+static struct rcar_vin_platform_data vin##idx##_pdata = {		\
+	.flags = flag,							\
+};									\
+									\
+static struct soc_camera_link cam##idx##_link = {			\
+	.bus_id = idx,							\
+	.board_info = &i2c_cam##idx##_device,				\
+	.i2c_adapter_id = 2,						\
+	.module_name = name,						\
+	.priv = pdata,							\
+}
+
+KOELSCH_CAMERA(0, "adv7612", 0x4c, NULL, RCAR_VIN_BT709);
+KOELSCH_CAMERA(1, "adv7180", 0x20, NULL, RCAR_VIN_BT656);
+
+static void __init lager_add_camera0_device(void)
+{
+	platform_device_register_data(&platform_bus, "soc-camera-pdrv", 0,
+				      &cam0_link, sizeof(cam0_link));
+	lager_add_vin_device(0, &vin0_pdata);
+}
+
+static void __init lager_add_camera1_device(void)
+{
+	platform_device_register_data(&platform_bus, "soc-camera-pdrv", 1,
+				      &cam1_link, sizeof(cam1_link));
+	lager_add_vin_device(1, &vin1_pdata);
+}
+
 /* VSP1 */
 static const struct vsp1_platform_data lager_vspr_pdata __initconst = {
 	.features = VSP1_HAS_LUT | VSP1_HAS_SRU,
@@ -602,6 +669,8 @@ static void __init lager_add_standard_devices(void)
 	lager_add_usb1_device();
 	lager_add_usb2_device();
 	lager_add_rsnd_device();
+	lager_add_camera0_device();
+	lager_add_camera1_device();
 	lager_add_vsp1_devices();
 	lager_add_msiof_device(spi_bus, ARRAY_SIZE(spi_bus));
 }
