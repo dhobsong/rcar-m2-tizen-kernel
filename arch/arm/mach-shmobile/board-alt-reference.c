f8d427e5914c8005f681e1c1edf766e1d5095844
Shinobu Uehara <shinobu.uehara.xc@renesas.com>
2014-05-23 14:32:26 +0900

    ARM: shmobile: alt-reference: Add PCI USB host driver support
    Signed-off-by: Shinobu Uehara <shinobu.uehara.xc@renesas.com>

--- arch/arm/mach-shmobile/board-alt-reference.c
+++ arch/arm/mach-shmobile/board-alt-reference.c
@@ -62,6 +62,7 @@ static const struct clk_name clk_names[] __initconst = {
 	{ "scifa4", NULL, "sh-sci.15" },
 	{ "scifa5", NULL, "sh-sci.16" },
 	{ "hscif2", NULL, "sh-sci.17" },
+	{ "hsusb", NULL, "usb_phy_rcar_gen2" },
 };
 
 /*
@@ -72,7 +73,40 @@ static const struct clk_name clk_enables[] __initconst = {
 	{ "mmcif0", NULL, "ee200000.mmc" },
 	{ "sdhi0", NULL, "ee100000.sd" },
 	{ "sdhi1", NULL, "ee140000.sd" },
+	{ "hsusb", NULL, "renesas_usbhs" },
+	{ "ehci", NULL, "pci-rcar-gen2.1" },
 };
+
+/* USBHS PHY */
+static const struct rcar_gen2_phy_platform_data usbhs_phy_pdata __initconst = {
+	.chan0_pci = 0,	/* Channel 0 is USBHS */
+	.chan2_pci = 1,	/* Channel 2 is PCI USB */
+};
+
+static const struct resource usbhs_phy_resources[] __initconst = {
+	DEFINE_RES_MEM(0xe6590100, 0x100),
+};
+
+/* Internal PCI1 */
+static const struct resource pci1_resources[] __initconst = {
+	DEFINE_RES_MEM(0xee0d0000, 0x10000),	/* CFG */
+	DEFINE_RES_MEM(0xee0c0000, 0x10000),	/* MEM */
+	DEFINE_RES_IRQ(gic_spi(113)),
+};
+
+static const struct platform_device_info pci1_info __initconst = {
+	.parent		= &platform_bus,
+	.name		= "pci-rcar-gen2",
+	.id		= 1,
+	.res		= pci1_resources,
+	.num_res	= ARRAY_SIZE(pci1_resources),
+	.dma_mask	= DMA_BIT_MASK(32),
+};
+
+static void __init alt_add_usb1_device(void)
+{
+	platform_device_register_full(&pci1_info);
+}
 
 static void __init alt_add_standard_devices(void)
 {
@@ -80,6 +114,13 @@ static void __init alt_add_standard_devices(void)
 	shmobile_clk_workaround(clk_enables, ARRAY_SIZE(clk_enables), true);
 	r8a7794_add_dt_devices();
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+
+	platform_device_register_resndata(&platform_bus, "usb_phy_rcar_gen2",
+					  -1, usbhs_phy_resources,
+					  ARRAY_SIZE(usbhs_phy_resources),
+					  &usbhs_phy_pdata,
+					  sizeof(usbhs_phy_pdata));
+	alt_add_usb1_device();
 }
 
 static const char * const alt_boards_compat_dt[] __initconst = {
