385ec410683b4a87550f70d9cdfc7a6a12c4d2dc
Geert Uytterhoeven <geert+renesas@glider.be>
2014-05-26 14:05:25 +0200

    spi: core: Ignore unsupported spi-[tr]x-bus-width property values
    Rejecting unsupported values of spi-tx-bus-width and spi-rx-bus-width
    may break compatibility with future DTs. Just ignore them, falling back
    to Single SPI Transfers.
    
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    (cherry picked from commit 80874d8c9e9cbfa195572dfea46f2314098b3869)
    Signed-off-by: Simon Horman <horms+renesas@verge.net.au>
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>
    
    Conflicts:
    	drivers/spi/spi.c
    
    (cherry picked from commit 9c6e50a8652777a64fa7642cd37155c25087d29b)
    Signed-off-by: Ryo Kataoka <ryo.kataoka.wt@renesas.com>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1153,11 +1153,10 @@ static void of_register_spi_devices(struct spi_master *master)
 				spi->mode |= SPI_TX_QUAD;
 				break;
 			default:
-				dev_err(&master->dev,
-					"spi-tx-bus-width %d not supported\n",
-					be32_to_cpup(prop));
-				spi_dev_put(spi);
-				continue;
+				dev_warn(&master->dev,
+					 "spi-tx-bus-width %d not supported\n",
+					 be32_to_cpup(prop));
+				break;
 			}
 		}
 
@@ -1173,11 +1172,10 @@ static void of_register_spi_devices(struct spi_master *master)
 				spi->mode |= SPI_RX_QUAD;
 				break;
 			default:
-				dev_err(&master->dev,
-					"spi-rx-bus-width %d not supported\n",
-					be32_to_cpup(prop));
-				spi_dev_put(spi);
-				continue;
+				dev_warn(&master->dev,
+					 "spi-rx-bus-width %d not supported\n",
+					 be32_to_cpup(prop));
+				break;
 			}
 		}
 
